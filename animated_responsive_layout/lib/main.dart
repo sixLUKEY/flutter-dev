e16f387b780f6696f7932eca91ebd8ae09b6252d
sixLUKEY <marcoslukerojo@gmail.com>
2024-05-30 15:10:07 +0200

    Animation transitions progress

--- animated_responsive_layout/lib/main.dart
+++ animated_responsive_layout/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'models/data.dart' as data;
 import 'models/models.dart';
+import 'widgets/disappearing_bottom_navigation_bar.dart';
+import 'widgets/disappearing_navigation_rail.dart';
 import 'widgets/email_list_view.dart';
 
 void main() {
@@ -37,21 +39,67 @@ class _FeedState extends State<Feed> {
   late final _backgroundColor = Color.alphaBlend(
       _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
 
+  int selectedIndex = 0;
+
+  bool wideScreen = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final double width = MediaQuery.of(context).size.width;
+    wideScreen = width > 600;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: _backgroundColor,
-        child: EmailListView(
-          currentUser: widget.currentUser,
-        ),
+      body: Row(
+        children: [
+          if (wideScreen)
+            DisappearingNavigationRail(
+              selectedIndex: selectedIndex,
+              backgroundColor: _backgroundColor,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+          Expanded(
+            child: Container(
+              color: _backgroundColor,
+              child: EmailListView(
+                selectedIndex: selectedIndex,
+                onSelected: (index) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
+                currentUser: widget.currentUser,
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: _colorScheme.tertiaryContainer,
-        foregroundColor: _colorScheme.onTertiaryContainer,
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: wideScreen
+          ? null
+          : FloatingActionButton(
+              backgroundColor: _colorScheme.tertiaryContainer,
+              foregroundColor: _colorScheme.onTertiaryContainer,
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+      bottomNavigationBar: wideScreen
+          ? null
+          : DisappearingBottomNavigationBar(
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
     );
   }
 }

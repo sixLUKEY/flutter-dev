1b1f8353cfc95b3a05db75df0d9d080fb644c5a8
sixLUKEY <marcoslukerojo@gmail.com>
2024-07-25 13:52:37 +0200

    (#15): Scaling components

---
+++ 4webdev/for_web_devs/lib/manipulating_position_size/scaling_components/scaling.svelte
@@ -0,0 +1,22 @@
+<div class="grey-box">
+  <div class="red-box">Lorem Ipsum</div>
+</div>
+
+<style>
+  .grey-box {
+    background-color: #e0e0e0; /* grey 300 */
+    width: 320px;
+    height: 240px;
+    font: 900 24px Roboto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .red-box {
+    background-color: #ef5350;
+    padding: 16px;
+    color: #ffffff;
+    transform: scale(1.5);
+  }
+</style>

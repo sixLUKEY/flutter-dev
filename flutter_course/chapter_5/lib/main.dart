61845dbaca41c973d6b0df9c221e308613a66256
sixLUKEY <marcoslukerojo@gmail.com>
2024-07-29 11:41:49 +0200

    (#13): Chapter 5 - Added Application Theme

--- flutter_course/chapter_5/lib/main.dart
+++ flutter_course/chapter_5/lib/main.dart
@@ -3,8 +3,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: ThemeColors.whiteText,
+          centerTitle: false,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.all(8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: const TextStyle().copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+                fontSize: 24,
+              ),
+            ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
+
+class ThemeColors {
+  static Color primary = Colors.lightBlue[400]!;
+  static Color whiteText = const Color.fromARGB(220, 255, 255, 255);
+}
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: ThemeColors.primary,
+);

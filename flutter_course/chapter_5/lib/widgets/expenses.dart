3febb2e5831f1bd434d5ca1bfe8b0097af4674d6
sixLUKEY <marcoslukerojo@gmail.com>
2024-07-30 22:12:22 +0200

    (#13): Chapter 6 - Basic media query added to chart

--- flutter_course/chapter_5/lib/widgets/expenses.dart
+++ flutter_course/chapter_5/lib/widgets/expenses.dart
@@ -77,6 +77,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses. Start adding!'),
     );
@@ -100,16 +102,29 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(
-            expenses: _registeredExpenses,
-          ),
-          Expanded(
-            child: mainContent,
-          ),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(
+                  expenses: _registeredExpenses,
+                ),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(
+                    expenses: _registeredExpenses,
+                  ),
+                ),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            ),
     );
   }
 }

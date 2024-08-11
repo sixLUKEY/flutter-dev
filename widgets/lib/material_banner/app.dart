7f7ae44e46a7e3fca41a7847e8043363b84ce018
sixLUKEY <marcoslukerojo@gmail.com>
2024-08-11 19:55:01 +0200

    Flutter Widgets : MaterialBanner

---
+++ widgets/lib/material_banner/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CMaterialBanner extends StatelessWidget {
+  const CMaterialBanner({
+    super.key,
+  });
+
+  @override
+  Widget build(context) {
+    return Center();
+  }
+}
